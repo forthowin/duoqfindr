10b33a6a1d32cc5e07ab98d9decaaa02afe736b7
Tho Nguyen <thogotmail@gmail.com>
2015-02-26 13:43:03 -0600

    Add min/max username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 15}
   validates :password, length: {minimum: 5}
 
 end
