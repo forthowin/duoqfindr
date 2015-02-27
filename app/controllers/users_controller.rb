93fde48e53130601872c8a712f557c61ab1686a3
Tho Nguyen <thogotmail@gmail.com>
2015-02-26 18:47:14 -0600

    Clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  #before_action :set_ip, only: [:create, :update]
 
   def new
     @user = User.new
