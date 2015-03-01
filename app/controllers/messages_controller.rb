2a7ee54454192eaf2a1bde48e08fd7c4ce22f032
Tho Nguyen <thogotmail@gmail.com>
2015-03-01 00:27:58 -0600

    Validation for empty input

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,18 +4,26 @@ class MessagesController < ApplicationController
   def create
     user = User.find_by username: params[:username]
 
-    current_user.send_message(user, params[:body], params[:subject])
-
-    flash[:notice] = "Message sent!"
-    redirect_to user_path(user)
+    if params[:body].blank? or params[:subject].blank?
+      flash[:danger] = "Subject and body must be present."
+      redirect_to :back
+    else
+      current_user.send_message(user, params[:body], params[:subject])
+      flash[:notice] = "Message sent!"
+      redirect_to user_path(user)
+    end
   end
 
   def reply
     conversation = current_user.mailbox.conversations.find(params[:id])
 
-    current_user.reply_to_conversation(conversation, params[:reply_body])
-
-    flash[:notice] = "Message sent!"
-    redirect_to conversation_path(conversation)
+    if params[:reply_body].blank?
+      flash[:danger] = "Reply body can't be blank"
+      redirect_to :back
+    else
+      current_user.reply_to_conversation(conversation, params[:reply_body])
+      flash[:notice] = "Message sent!"
+      redirect_to conversation_path(conversation)
+    end
   end
 end
