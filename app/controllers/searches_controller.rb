ff6c29eac40ff88af1bfde832d8f5a921b23587c
Tho Nguyen <thogotmail@gmail.com>
2015-03-01 22:47:15 -0600

    Refactor search logic

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,12 +8,11 @@ class SearchesController < ApplicationController
   end
 
   def update
-    if params[:radius] and params[:radius].to_i >= MIN_RADIUS and params[:radius].to_i <= MAX_RADIUS
+    if params[:radius].to_i.between?(MIN_RADIUS, MAX_RADIUS)
       @users = current_user.nearbys(params[:radius].to_i)
-      render :show
-    elsif params[:radius] and (params[:radius].to_i < MIN_RADIUS or params[:radius].to_i > MAX_RADIUS)
+    else
       flash.now[:danger] = "Value must be between 1-150."
-      render :show
     end
+    render :show
   end
 end
