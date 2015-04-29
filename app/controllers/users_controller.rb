eaa920f53ff25aa06d2fcd33012ccd9232c63c2f
Tho Nguyen <thogotmail@gmail.com>
2015-04-29 11:39:24 -0500

    change create redirect to edit user path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "You have successfully registered."
-      redirect_to root_path
+      redirect_to edit_user_path @user
     else
       render :new
     end
