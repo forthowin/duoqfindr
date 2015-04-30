417429fb63b806a9e13fcc781d209a90d4852899
Tho Nguyen <thogotmail@gmail.com>
2015-04-29 21:55:03 -0500

    finish test for searches controller

---
+++ spec/controllers/searches_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe SearchesController do
+  describe 'POST update' do
+    it 'redirects to the sign in page for unauthorized users' do
+      post :update, radius: "100"
+      expect(response).to redirect_to login_path
+    end
+
+    it 'renders the show page' do
+      set_current_user
+      post :update, radius: "100"
+      expect(response).to render_template :show
+    end
+
+    it 'sets the flash danger message for invalid inputs' do
+      set_current_user
+      post :update, radius: "100"
+      expect(flash[:danger]).to be_present
+    end
+
+    it 'assigns users for valid inputs' do
+      bob = Fabricate(:user, latitude: 0, longitude: 0)
+      tim = Fabricate(:user, latitude: 0, longitude: 0)
+      jim = Fabricate(:user, latitude: 0, longitude: 0)
+      set_current_user(bob)
+      post :update, radius: "100"
+      expect(assigns(:users)).to eq([tim, jim])
+    end
+  end
+end
