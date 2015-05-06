3c5519bc7bc5f6799d10c7bc532a5e5927ff2b30
Tho Nguyen <thogotmail@gmail.com>
2015-05-06 00:25:30 -0500

    add token and account link action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user, only: [:edit, :update]
+  before_action :require_user, only: [:edit, :update, :link_account, :token]
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:edit, :update]
 
@@ -40,6 +40,25 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.js
     end
+  end
+
+  def link_account
+    begin
+      account_response = JSON.parse(open("https://na.api.pvp.net/api/lol/#{params[:region]}/v1.4/summoner/by-name/#{params[:summoner_name]}?api_key=#{ENV['RIOT_API_KEY']}").read)
+      summoner_id = account_response[params[:summoner_name]]['id']
+
+      runes_response = JSON.parse(open("https://na.api.pvp.net/api/lol/na/v1.4/summoner/#{summoner_id}/runes?api_key=#{ENV['RIOT_API_KEY']}").read)
+
+      if current_user.account_token == runes_response[summoner_id.to_s]['pages'].first['name']
+        current_user.update_column(:summoner_id, account_response[params[:summoner_name]]['id'])
+        flash[:success] = 'Your account was linked successfully!'
+      else
+        flash[:danger] = 'Token did not match or runepage has not been updated yet. Verify account again in a bit.'
+      end
+    rescue OpenURI::HTTPError => e
+      flash[:danger] = e.message
+    end
+    redirect_to :back
   end
 
   private
