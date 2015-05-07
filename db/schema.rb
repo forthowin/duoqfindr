cadb933b749df6cf0a5f61661de0e7a9aa0af610
Tho Nguyen <thogotmail@gmail.com>
2015-05-07 03:07:42 -0500

    add email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507053549) do
+ActiveRecord::Schema.define(version: 20150507080637) do
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.integer "unsubscriber_id"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150507053549) do
     t.string  "account_token"
     t.integer "summoner_id"
     t.string  "region"
+    t.string  "email"
   end
 
 end
