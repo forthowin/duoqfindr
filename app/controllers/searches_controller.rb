88e09508dda1be637d77dc1b6ef8ac426ed56a32
Tho Nguyen <thogotmail@gmail.com>
2015-05-07 19:36:55 -0500

    set flash notice if no match found

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,6 +10,7 @@ class SearchesController < ApplicationController
   def update
     if params[:radius].to_i.between?(MIN_RADIUS, MAX_RADIUS)
       @users = current_user.nearbys(params[:radius].to_i)
+      flash.now[:info] = 'No match found. Try again later when more players sign up.'
     else
       flash.now[:danger] = "Value must be between #{MIN_RADIUS}-#{MAX_RADIUS}."
     end
