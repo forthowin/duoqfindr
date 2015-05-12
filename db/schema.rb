e3cf84099be8474e2b29ff58129dfedabf1c0c7d
Tho Nguyen <thogotmail@gmail.com>
2015-05-12 15:55:15 -0500

    add terms attribute to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507080637) do
+ActiveRecord::Schema.define(version: 20150512201108) do
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.integer "unsubscriber_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150507080637) do
     t.integer "summoner_id"
     t.string  "region"
     t.string  "email"
+    t.integer "terms"
   end
 
 end
