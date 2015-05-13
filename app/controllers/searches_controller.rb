fd7224f65ee98449c58a53ec07a6065671dd3347
Tho Nguyen <thogotmail@gmail.com>
2015-05-13 17:33:47 -0500

    fix flash message bug

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,7 +8,7 @@ class SearchesController < ApplicationController
     if params[:radius].to_i.between?(MIN_RADIUS, MAX_RADIUS)
       @users = current_user.nearbys(params[:radius].to_i).paginate(:page => params[:page])
       flash.now[:info] = 'No match found. Try again later when more players sign up.' unless @users.present?
-    else
+    elsif params[:radius].present?
       flash.now[:danger] = "Value must be between #{MIN_RADIUS} and #{MAX_RADIUS}."
     end
     render :show
