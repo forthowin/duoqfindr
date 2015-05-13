213c0dbb8197ad5dbe315e73ca9ea5f5d31cc8eb
Tho Nguyen <thogotmail@gmail.com>
2015-05-13 17:11:38 -0500

    change search to get action

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,11 +5,8 @@ class SearchesController < ApplicationController
   before_action :require_user
 
   def show
-  end
-
-  def update
     if params[:radius].to_i.between?(MIN_RADIUS, MAX_RADIUS)
-      @users = current_user.nearbys(params[:radius].to_i)
+      @users = current_user.nearbys(params[:radius].to_i).paginate(:page => params[:page])
       flash.now[:info] = 'No match found. Try again later when more players sign up.' unless @users.present?
     else
       flash.now[:danger] = "Value must be between #{MIN_RADIUS} and #{MAX_RADIUS}."
--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe SearchesController do
-  describe 'POST update' do
+  describe 'GET show' do
     it 'redirects to the sign in page for unauthorized users' do
-      post :update, radius: "100"
+      get :show, radius: "100"
       expect(response).to redirect_to login_path
     end
 
     it 'renders the show page' do
       set_current_user
-      post :update, radius: "100"
+      get :show, radius: "100"
       expect(response).to render_template :show
     end
 
     it 'sets the flash danger message for invalid inputs' do
       set_current_user
-      post :update, radius: "160"
+      get :show, radius: "160"
       expect(flash[:danger]).to be_present
     end
 
@@ -24,14 +24,14 @@ describe SearchesController do
       tim = Fabricate(:user, latitude: 0, longitude: 0)
       jim = Fabricate(:user, latitude: 0, longitude: 0)
       set_current_user(bob)
-      post :update, radius: "100"
+      get :show, radius: "100"
       expect(assigns(:users)).to eq([tim, jim])
     end
 
     it 'sets the flash info message when no one was found' do
       bob = Fabricate(:user, latitude: 0, longitude: 0)
       set_current_user(bob)
-      post :update, radius: '100'
+      get :show, radius: '100'
       expect(flash[:info]).to be_present
     end
   end
